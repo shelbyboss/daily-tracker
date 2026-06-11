013d0cfe7a15c3a46ff243aea159ccae71a00380
shelbyboss <chickbossshelby@gmail.com>
2026-06-12 00:22:37 +0700

    Update main.jsx

--- daily-tracker/src/main.jsx
+++ daily-tracker/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Stats from './Stats.jsx'
+
+const path = window.location.pathname;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {path === '/stats' ? <Stats /> : <App />}
   </React.StrictMode>,
 )
